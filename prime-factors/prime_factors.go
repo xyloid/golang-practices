bad17a9e87845eb26f225c3e938639a0571c1353
yilin xu <xu.yilin@husky.neu.edu>
2020-08-10 15:40:18 -0400

    factorization of large number is very slow

--- prime-factors/prime_factors.go
+++ prime-factors/prime_factors.go
@@ -1,1 +1,36 @@
+// Package prime imlements function factor
 package prime
+
+import "fmt"
+
+// Factors return all the prime factors of a give number
+func Factors(n int64) []int64 {
+	fmt.Println(n)
+	ret := make([]int64, 0)
+	for factor := int64(2); factor <= n; {
+
+		if isPrime(factor) && n%factor == 0 {
+			ret = append(ret, factor)
+			n = n / factor
+		} else {
+			factor++
+		}
+	}
+	return ret
+}
+
+func isPrime(n int64) bool {
+	if n == 2 {
+		return true
+	}
+	if n%2 == 0 || n < 2 {
+		return false
+	}
+
+	for i := int64(3); i < n; i += 2 {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
