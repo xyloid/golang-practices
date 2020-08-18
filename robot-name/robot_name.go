afb2dae83f4fad003b2b6ab91e98c1f8c28eac7a
yilin xu <xu.yilin@husky.neu.edu>
2020-08-18 13:11:57 -0400

    pass the normal tests

--- robot-name/robot_name.go
+++ robot-name/robot_name.go
@@ -1,1 +1,44 @@
+// Package robotname implements an random robot name generator.
 package robotname
+
+import (
+	"math/rand"
+	"time"
+)
+
+// Robot types store infomation about a robot.
+type Robot struct {
+	name string
+}
+
+var digits = []rune("0123456789")
+
+var letters = []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+// Name returns a formatted robot name.
+func (r *Robot) Name() (string, error) {
+	if r.name == "" {
+		r.name = r.randName()
+	}
+
+	return r.name, nil
+}
+
+// Reset generates a new name for the robot.
+func (r *Robot) Reset() {
+	r.name = r.randName()
+}
+
+func (r *Robot) randName() string {
+	randRune := make([]rune, 5)
+	for i := range randRune {
+		rand.Seed(time.Now().UnixNano())
+		if i < 2 {
+			randRune[i] = letters[rand.Intn(len(letters))]
+		} else {
+			randRune[i] = digits[rand.Intn(len(digits))]
+		}
+	}
+
+	return string(randRune)
+}
